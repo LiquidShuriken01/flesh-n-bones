d0d3452ce1f8e106237d10ee458492ed570577dd
PeterMamaev <145819568+PeterMamaev@users.noreply.github.com>
2023-11-14 18:29:32 -0500

    Terrain

---
+++ Assets/Scripts/Misc/SceneTransition.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManager;
+
+public class SceneTransition : MonoBehaviour
+/*{
+    public string NextScene;
+
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            SceneManager.LoadScene(NextScene);
+        }
+    }*/
+    
+    // Start is called before the first frame update
+{
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
