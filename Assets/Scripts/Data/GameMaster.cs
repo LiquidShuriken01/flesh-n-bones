db0fe6c91d3f6debdc5b62b7253400599a8252cc
LiquidShuriken01 <yliu28290@gmail.com>
2023-12-04 13:51:34 -0500

    Added a game log

--- Assets/Scripts/Data/GameMaster.cs
+++ Assets/Scripts/Data/GameMaster.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using static UnityEngine.Networking.UnityWebRequest;
 
 public class GameMaster : MonoBehaviour
 {
+    [SerializeField] private Transform text_area;
+    private TMP_Text log;
+
     public static GameMaster _instance { get; private set; }
 
     private void Awake()
@@ -14,6 +19,9 @@ public class GameMaster : MonoBehaviour
         }
         else
         {
+            log = text_area.GetComponent<TMP_Text>();
+            log.text = "";
+
             _instance = this;
         }
     }
@@ -21,6 +29,7 @@ public class GameMaster : MonoBehaviour
     private int Roll(int threshold, int modifier)
     {
         int result = Random.Range(1, 100) + modifier;
+        log.text = log.text + "\n" + $"Rolled {result} vs. {threshold}";
         Debug.Log($"Rolled {result} vs. {threshold}");
         if (result < threshold - 25) { return 0; }
         else if (result < threshold) { return 1; }
@@ -48,21 +57,25 @@ public class GameMaster : MonoBehaviour
         switch (Roll(target.GetStatValueInt(target_stat), acc))
         {
             case 0:
+                log.text = log.text + "\n" + $"Miss...";
                 Debug.Log($"Miss...");
                 break;
             case 1:
                 dmg = Mathf.RoundToInt(damage * 0.5f);
-                Debug.Log($"Grazing Hit. {name} deals {dmg} damage to {target.name}.");
+                log.text = log.text + "\n" + $"Grazing Hit. {name} deals {dmg} damage to {target.char_name}.";
+                Debug.Log($"Grazing Hit. {name} deals {dmg} damage to {target.char_name}.");
                 target.TakeDamage(dmg);
                 break;
             case 2:
                 dmg = Mathf.RoundToInt(damage);
-                Debug.Log($"Hit. {name} deals {dmg} damage to {target.name}.");
+                log.text = log.text + "\n" + $"Hit. {name} deals {dmg} damage to {target.char_name}.";
+                Debug.Log($"Hit. {name} deals {dmg} damage to {target.char_name}.");
                 target.TakeDamage(dmg);
                 break;
             case 3:
                 dmg = Mathf.RoundToInt(damage * 1.5f);
-                Debug.Log($"Critical Hit! {name} deals {dmg} damage to {target.name}.");
+                log.text = log.text + "\n" + $"Critical Hit! {name} deals {dmg} damage to {target.char_name}.";
+                Debug.Log($"Critical Hit! {name} deals {dmg} damage to {target.char_name}.");
                 target.TakeDamage(dmg);
                 break;
             default: break;
