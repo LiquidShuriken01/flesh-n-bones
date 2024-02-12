9fb1ffe77ff02268857f5031d799cfd1b43b368e
LiquidShuriken01 <yliu28290@gmail.com>
2024-02-11 19:52:31 -0500

    Update Sprite.cs
    Added sprite pivot styles

--- Assets/Scripts/Misc/Sprite.cs
+++ Assets/Scripts/Misc/Sprite.cs
@@ -3,9 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations;
 
+public enum TrackingStyle
+{
+    Precise,
+    Reduced,
+    Upright,
+    None
+}
+
 public class Sprite : MonoBehaviour
 {
     private Transform target;
+    public TrackingStyle tracking_style;
 
     private void Start()
     {
@@ -15,6 +24,18 @@ public class Sprite : MonoBehaviour
     private void Update()
     {
         Vector3 reversed_target = 3 * transform.position - 2 * target.position;
+        switch (tracking_style)
+        {
+            case TrackingStyle.Precise:
+                break;
+            case TrackingStyle.Reduced:
+                reversed_target.x /= 2;
+                break;
+            case TrackingStyle.Upright:
+                reversed_target.x = 0;
+                break;
+            default: break;
+        }
         transform.LookAt(reversed_target);
     }
 }
