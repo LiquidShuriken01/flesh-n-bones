676fe4090b0cc2c9c0f9f1db27a9ab0923fbc916
LiquidShuriken01 <yliu28290@gmail.com>
2024-02-14 20:54:29 -0500

    working player health bar

--- Assets/Scripts/UI/UIControl.cs
+++ Assets/Scripts/UI/UIControl.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIControl : MonoBehaviour
 {
     GameObject inventory;
+    GameObject health_bar;
     GameObject reticle;
     GameObject pause_text;
+    PlayerInfo player_data;
+    Slider hp_slider;
     bool inv_is_active = false;
     bool show_reticle = true;
     bool paused = false;
@@ -14,11 +18,15 @@ public class UIControl : MonoBehaviour
     void Start()
     {
         inventory = transform.GetChild(0).gameObject;
+        health_bar = transform.GetChild(1).gameObject;
         reticle = transform.GetChild(2).gameObject;
         pause_text = transform.GetChild(3).gameObject;
+        hp_slider = health_bar.GetComponent<Slider>();
         inventory.SetActive(inv_is_active);
         reticle.SetActive(show_reticle);
         pause_text.SetActive(paused);
+
+        player_data = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDataHandler>().player_info;
     }
 
     void Update()
@@ -38,5 +46,8 @@ public class UIControl : MonoBehaviour
             pause_text.SetActive(paused);
             reticle.SetActive(show_reticle);
         }
+
+        hp_slider.maxValue = player_data.max_health;
+        hp_slider.value = player_data.health;
     }
 }
